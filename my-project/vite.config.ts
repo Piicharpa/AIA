8c4d3d4875255874f33539556661b671ee62c79d
Piicharpa <Neuah2515@gmail.com>
2025-07-13 02:13:21 +0700

    project setup

---
+++ my-project/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "/aia/",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
